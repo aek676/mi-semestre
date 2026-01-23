70db90fc40078d2de3545b25d6127828974146aa
aek676 <aek676@inlumine.ual.es>
2026-01-23 16:07:12 +0100

    fix: update API_BASE_URL to PUBLIC_API_BASE_URL in env.d.ts for consistency

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,7 +1,7 @@
 interface ImportMetaEnv {
-    readonly API_BASE_URL?: string;
+  readonly PUBLIC_API_BASE_URL?: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
